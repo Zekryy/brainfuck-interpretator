9d1f07cc4491584497e8ae08045240f2ce3ae13a
Pedro Brito <36624303+Zekryy@users.noreply.github.com>
2024-05-01 18:14:18 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  BrainFuck Interpretator
+//
+//  Created by zekry
+//
+
+#include<string>
+#include<stack>
+
+int main() {
+    char memory[30000] = {};
+    std::string script = "+++++>>+>+>+>++++++++>+>+>+>++++++>+>+>+>+++>+>+>+>+++++++++>+>+>+>+>+>+>+><[<]<[->>>>>>>>>[>->->-<<<<<<<<-<<->>>[>>>>>+>+<<<<<<-]>>>>>[<<<<<+>>>>>-]<[>+>>+<<<-]>[<+>-]<<<<<[->>>>-[>]<<<<<]<[->>[-]>>>>>>[<<+>>-]>[<<<<<<<+>>>>>>>-]<<<<<<<<<]<[->>>>>>[-]>>[<<<<<<+>>>>>>-]>[<<<+>>>-]<<<<<<<<<<]+>+>+>>>>>>+>+>+<<<<<<<>>>>>>>>]<<<<[<]<]>>>>>[++++++++++++++++++++++++++++++++++++++++++++++++.>>>>]";
+    int i = 0;
+    std::stack<int> while_open;
+    int *next = new int[script.size()], *prev = new int[script.size()];
+    while(i != script.size()){
+        next[i] = i+1;
+        prev[i] = i-1;
+        if(script[i] == '['){
+            while_open.push(i);
+        }
+        else if(script[i] == ']'){
+            int p = while_open.top();
+            while_open.pop();
+            prev[i] = p;
+            next[p] = i;
+        }
+        i++;
+    }
+    i = 0;
+    char *p = &memory[0];
+    while(i != script.size()){
+        if(script[i] == '>') p++;
+        else if(script[i] == '<') p--;
+        else if(script[i] == '+') (*p)++;
+        else if(script[i] == '-') (*p)--;
+        else if(script[i] == '[') {
+            if(*p == 0) i = next[i];
+        }
+        else if(script[i] == ']') i = prev[i] - 1;
+        else if(script[i] == '.') putchar(*p);
+        else if(script[i] == ',') *p = getchar();
+        i++;
+    }
+    return 0;
+}
